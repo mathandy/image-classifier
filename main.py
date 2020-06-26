7d6dd81527efd2b6d94d6bbd92a93d34b59c024d
Andy Port <andyaport@gmail.com>
2020-06-25 22:27:46 -0700

    implemented grad tape training loop (untested)

--- main.py
+++ main.py
@@ -58,6 +58,95 @@ def prepare_data(args):
     return ds_train, ds_val, ds_test, class_names
 
 
+class Classifier:
+    def __init__(self, model, optimizer, loss, metric_dict, class_weights, args):
+        self.model = model
+        self.optimizer = optimizer
+        self.loss = loss
+        self.class_weights = class_weights
+        self.metric_dict = metric_dict
+        self.args = args
+
+    def get_report(self, results, title=None):
+        report = ''
+        if title is not None:
+            report =  title + '\n'
+        report += '\n'.join(f'{k}: {v}' for k, v in results.items())
+        return f'\n{report}\n'
+
+    def update_metrics(self, y_true, y_pred):
+        for metric in self.metric_dict.values():
+            metric.update_state(y_true, y_pred)
+
+    def get_metric_results(self):
+        results = {}
+        for metric_name, metric in self.metric_dict:
+            results[metric_name] = metric.result()
+        return results
+
+    def reset_metrics(self):
+        for metric in self.metric_dict.values():
+            metric.reset_states()
+
+    def train_step(self, x_batch, y_batch, sample_weights=None):
+        with tf.GradientTape() as tape:
+            # ANDY - figure out if this updates batch norm layers even
+            # though they are marked not trainable
+            # TO SEE, check self.model.trainable_weights
+            logits = self.model(x_batch, training=True)
+            loss_value = self.loss(y_batch, logits)
+        grads = tape.gradient(loss_value, self.model.trainable_weights)
+        if sample_weights is not None:
+            grads = grads * sample_weights
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
+        return logits
+
+    def score(self, batches, reset=True):
+        self.reset_metrics()
+        for x_batch, y_batch in batches:
+            logits = self.model(x_batch, training=False)
+            predictions = tf.argmax(logits, 1)
+            self.loss.update_state(y_batch, predictions)
+            self.update_metrics(y_batch, predictions)
+        results = self.get_metric_results()
+        self.reset_metrics()
+        return results
+
+    def compute_sample_weights(self, y_true):
+        if self.class_weights is None:
+            return None
+        raise NotImplementedError
+
+    def train(self, training_data, validation_data=None, class_weights=None):
+        epochs = 2
+        for epoch in range(epochs):
+            print("\nStart of epoch %d" % (epoch,))
+            epoch_train_loss = 0
+            for step, (x_train_batch, y_train_batch) in enumerate(training_data):
+                if class_weights is None:
+                    sample_weights = None
+                else:
+                    raise NotImplementedError
+                train_batch_logits = self.train_step(
+                    x_batch=x_train_batch,
+                    y_batch=y_train_batch,
+                    sample_weights=self.compute_sample_weights(y_train_batch)
+                )
+                train_batch_predictions = tf.argmax(train_batch_logits, 1)
+                epoch_train_loss += \
+                    self.update_metrics(y_train_batch, train_batch_predictions)
+            train_results = self.get_metric_results()
+            self.reset_metrics()
+            train_results.update({'Loss': epoch_train_loss})
+            print(self.get_report(train_results, "Training Results"))
+
+            if validation_data is not None:
+                self.score(validation_data)
+                val_results = self.get_metric_results()
+                self.reset_metrics()
+                print(self.get_report(val_results, "Validation Results"))
+
+
 def main(args):
 
     # prepare data
@@ -69,31 +158,34 @@ def main(args):
     ds_val = ds_val.batch(args.batch_size)
     # ds_test = ds_test.batch(args.batch_size)
 
-    # define metrics
-    metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
-    # for class_id, class_name in enumerate(class_names):
-    #     metrics.append(tf.keras.metrics.Precision(
-    #         class_id=class_id, name='pr_' + class_name))
-    #     metrics.append(tf.keras.metrics.Recall(
-    #         class_id=class_id, name='re_' + class_name))
+    # set class weights to compensate for class imbalance
+    class_weights = None
+    if args.balance_class_weights:
+        raise NotImplementedError
 
-    # class_weights = dict((k, ))
+    # define metrics
+    metrics = {'Accuracy': tf.keras.metrics.SparseCategoricalAccuracy()}
 
     # build model
     model = build_model(model_name=args.model, n_classes=len(class_names))
-    model.compile(
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    classifier = Classifier(
+        model=model,
         optimizer=tf.keras.optimizers.Adam(),
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=metrics,
+        loss=loss,
+        metric_dict=metrics,
+        args=args,
     )
 
     # train model
-    model.fit(ds_train,
-              batch_size=args.batch_size,
-              validation_data=ds_val,
-              class_weight=None,
-              epochs=100)
+    classifier.train(
+        training_data=ds_train,
+        validation_data=ds_val,
+        class_weights=class_weights,
+    )
 
+    # test
+    classifier.score(ds_test)
 
 if __name__ == '__main__':
     from config import get_user_args
