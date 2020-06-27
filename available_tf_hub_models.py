41a2081505a3297f97bb28f58e26b913cd824b6c
Andy Port <andyaport@gmail.com>
2020-06-26 21:21:46 -0700

    add BiT models

--- available_tf_hub_models.py
+++ available_tf_hub_models.py
@@ -39,6 +39,10 @@ _mobilnet_models = [
     (35, 96),
 ]
 
+_efficientnet_models = [
+
+]
+
 TFHubModel = namedtuple('TF_Hub_Model', ['name', 'url', 'input_size'])
 
 
@@ -70,6 +74,29 @@ tf_hub_models += [
     tf_hub_model("resnet_v2_152", 224),
 ]
 
+# add efficientnet models
+# note using 114 instead of 224 to avoid OOM error
+def enet_url(i):
+    return f'https://tfhub.dev/tensorflow/efficientnet/b{i}/feature-vector/1'
+tf_hub_models += \
+    [tf_hub_model(f'EfficientNet-B{i}', 224, enet_url(i)) for i in range(8)]
+
+# add BiT-S (ImageNet-1k) and BiT-M (ImageNet-21k) models (expected size unkown)
+tf_hub_models += [
+    tf_hub_model('BiT-S-R50x1', 224, 'https://tfhub.dev/google/bit/s-r50x1/1'),
+    tf_hub_model('BiT-S-R50x3', 224, 'https://tfhub.dev/google/bit/m-r50x3/1'),
+    tf_hub_model('BiT-S-R101x1', 224, 'https://tfhub.dev/google/bit/s-r101x1/1'),
+    tf_hub_model('BiT-S-R101x3', 224, 'https://tfhub.dev/google/bit/s-r101x3/1'),
+    tf_hub_model('BiT-S-R152x4', 224, 'https://tfhub.dev/google/bit/s-r152x4/1'),
+
+    tf_hub_model('BiT-M-R50x1', 224, 'https://tfhub.dev/google/bit/m-r50x1/1'),
+    tf_hub_model('BiT-M-R50x3', 224, 'https://tfhub.dev/google/bit/m-r50x3/1'),
+    tf_hub_model('BiT-M-R101x1', 224, 'https://tfhub.dev/google/bit/m-r101x1/1'),
+    tf_hub_model('BiT-M-R101x3', 224, 'https://tfhub.dev/google/bit/m-r101x3/1'),
+    tf_hub_model('BiT-M-R152x4', 224, 'https://tfhub.dev/google/bit/m-r152x4/1'),
+]
+
+# make some convenient dictionaries
 tf_hub_model_urls = dict((name, url) for name, url, _ in tf_hub_models)
 tf_hub_model_input_size = dict((name, sz) for name, _, sz in tf_hub_models)
 
