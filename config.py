1662af45897aeb8c84a1305b6ae752dbeea887ff
Andy Port <andyaport@gmail.com>
2020-07-24 19:44:57 -0700

    fix doc typo

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from available_tf_hub_models import tf_hub_model_input_size
-from tempfile import gettempdir
 from time import time
 
 
@@ -10,8 +9,19 @@ def get_user_args():
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--image_dir', default='../bug-data/Multi_Inverts_Master', type=Path,
+        '--image_dir', default=Path('../bug-data/Multi_Inverts_Master'),
+        type=Path,
         help='Path to subdirectory-labeled image directory.'
+    )
+    parser.add_argument(
+        '--val_dir', default=None, type=Path,
+        help='Path to subdirectory-labeled image directory for validation.'
+             'Alternatively, use the val_part argument.'
+    )
+    parser.add_argument(
+        '--test_dir', default=None, type=Path,
+        help='Path to subdirectory-labeled image directory for testing.'
+             'Alternatively, use the test_part argument.'
     )
     parser.add_argument(
         '--val_part', default=0.1, type=float,
@@ -24,14 +34,12 @@ def get_user_args():
              'Alternatively, use the test_dir argument.'
     )
     parser.add_argument(
-        '--val_dir', default=None,
-        help='Path to subdirectory-labeled image directory for testing.'
-             'Alternatively, use the val_part argument.'
+        '--logdir', '-l', type=Path, default=None,  # default set below
+        help='Name of TF Hub model to use.'
     )
     parser.add_argument(
-        '--test_dir', default=None,
-        help='Path to subdirectory-labeled image directory for testing.'
-             'Alternatively, use the test_part argument.'
+        '--run_name', '-n', type=Path, default=None,  # default set below
+        help='Name of TF Hub model to use.'
     )
     parser.add_argument(
         '--batch_size', default=128, type=int,
@@ -54,14 +62,6 @@ def get_user_args():
         help='Do not use class weights to compensate for class imbalance.'
     )
     parser.add_argument(
-        '--logdir', '-l', type=Path, default=None, # default set below
-        help='Name of TF Hub model to use.'
-    )
-    parser.add_argument(
-        '--run_name', '-n', type=Path, default=None, # default set below
-        help='Name of TF Hub model to use.'
-    )
-    parser.add_argument(
         '--learning_rate', '-r', default=0.001, type=float,
         help='Name of TF Hub model to use.'
     )
@@ -71,10 +71,12 @@ def get_user_args():
 
 def process_args(args):
 
+    # image dimensions
     if args.image_dimensions is None:
         sz = tf_hub_model_input_size[args.model]
         args.image_dimensions = (sz, sz)
 
+    # logdir and run_name
     assert args.logdir is None or args.run_name is None
     if args.logdir is None:
         if args.run_name is None:
@@ -84,4 +86,9 @@ def process_args(args):
             warn("Using temp dir for named run.")
         args.logdir = Path('..', 'classifier-logs', args.run_name)
 
+    # train-val-test split parameters
+    if args.test_dir is not None:
+        args.test_part = 0
+    if args.val_dir is not None:
+        args.val_dir = 0
     return args
